7231925c9e1f7d821c9848592eb8399ffbd6c6d2
André Victor <andrehsvictor@gmail.com>
2024-03-08 19:22:43 -0300

    Add ResponseService class

---
+++ src/main/java/br/dcx/ufpb/apps4society/appalpharesponseregister/service/ResponseService.java
@@ -0,0 +1,20 @@
+package br.dcx.ufpb.apps4society.appalpharesponseregister.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.dcx.ufpb.apps4society.appalpharesponseregister.dto.ResponseDTO;
+import br.dcx.ufpb.apps4society.appalpharesponseregister.entity.Response;
+import br.dcx.ufpb.apps4society.appalpharesponseregister.repository.ResponseRepository;
+
+@Service
+public class ResponseService {
+
+    @Autowired
+    private ResponseRepository responseRepository;
+
+    public ResponseDTO getResponseById(long id) {
+        Response response = responseRepository.findById(id).orElseThrow(RuntimeException::new);
+        return new ResponseDTO(response.getId(), response.getTypedLetters(), response.isCorrect());
+    }
+}
