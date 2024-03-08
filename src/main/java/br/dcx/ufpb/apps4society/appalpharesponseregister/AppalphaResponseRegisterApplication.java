0b51a72329af11ac9bfe10ccf5e7e127286d08a2
André Victor <andrehsvictor@gmail.com>
2024-03-08 13:58:26 -0300

    Initial setup

---
+++ src/main/java/br/dcx/ufpb/apps4society/appalpharesponseregister/AppalphaResponseRegisterApplication.java
@@ -0,0 +1,13 @@
+package br.dcx.ufpb.apps4society.appalpharesponseregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppalphaResponseRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppalphaResponseRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dcx/ufpb/apps4society/appalpharesponseregister/AppalphaResponseRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dcx.ufpb.apps4society.appalpharesponseregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppalphaResponseRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
