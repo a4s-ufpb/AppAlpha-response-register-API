3c264ea9b1778cc0b0d26c4145f4a7507dd7b768
André Victor <andrehsvictor@gmail.com>
2024-03-14 19:54:47 -0300

    Add ResponseController with getAllResponses and getResponseById methods

---
+++ src/main/java/br/dcx/ufpb/apps4society/appalpharesponseregister/controller/ResponseController.java
@@ -0,0 +1,27 @@
+package br.dcx.ufpb.apps4society.appalpharesponseregister.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.dcx.ufpb.apps4society.appalpharesponseregister.service.ResponseService;
+
+@RestController
+@RequestMapping("/api/1.0")
+public class ResponseController {
+
+    @Autowired
+    private ResponseService responseService;
+
+    @GetMapping("/responses")
+    public ResponseEntity<?> getAllResponses() {
+        return ResponseEntity.ok(responseService.getAllResponses());
+    }
+
+    @GetMapping("/response/{id}")
+    public ResponseEntity<?> getResponseById(long id) {
+        return ResponseEntity.ok(responseService.getResponseById(id));
+    }
+}
