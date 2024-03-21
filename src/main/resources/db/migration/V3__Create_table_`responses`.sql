8a4d5075239475f4f10246b6e3587d3983369400
André Victor <andrehsvictor@gmail.com>
2024-03-21 12:00:17 +0000

    refactor: change the order of columns

--- src/main/resources/db/migration/V3__Create_table_`responses`.sql
+++ src/main/resources/db/migration/V3__Create_table_`responses`.sql
@@ -1,10 +1,10 @@
 create table if not exists responses (
     id serial primary key,
-    user_id integer not null,
-    question_id integer not null,
     typed_letters varchar(255) not null,
     response_date timestamp not null,
     correct boolean not null,
+    user_id integer not null,
+    question_id integer not null,
     foreign key (user_id) references users (id),
     foreign key (question_id) references questions (id)
 );
