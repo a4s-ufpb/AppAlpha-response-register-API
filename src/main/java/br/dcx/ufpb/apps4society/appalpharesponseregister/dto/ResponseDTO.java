66076fc5d6551a81613666017d6f39eecb98b178
André Victor <andrehsvictor@gmail.com>
2024-03-26 12:16:47 +0000

    feat: add UserDTO field

--- src/main/java/br/dcx/ufpb/apps4society/appalpharesponseregister/dto/ResponseDTO.java
+++ src/main/java/br/dcx/ufpb/apps4society/appalpharesponseregister/dto/ResponseDTO.java
@@ -1,13 +1,18 @@
 package br.dcx.ufpb.apps4society.appalpharesponseregister.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
+import br.dcx.ufpb.apps4society.appalpharesponseregister.view.Views;
+
+@JsonView(Views.Public.class)
 public class ResponseDTO {
     @JsonProperty(access = Access.READ_ONLY)
     private Long id;
     private String typedLetters;
     private boolean correct;
+    private UserDTO user;
 
     public ResponseDTO() {
     }
@@ -45,6 +50,14 @@ public class ResponseDTO {
 
     public void setCorrect(boolean correct) {
         this.correct = correct;
+    }
+
+    public UserDTO getUser() {
+        return user;
+    }
+
+    public void setUser(UserDTO user) {
+        this.user = user;
     }
 
 }
