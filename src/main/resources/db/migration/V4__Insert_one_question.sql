43a490ac653d0ff0d77ebd3b5c2f781a43ba9822
André Victor <andrehsvictor@gmail.com>
2024-06-13 13:27:11 -0300

    refactor: Remove theme validation from questions table creation

--- src/main/resources/db/migration/V4__Insert_one_question.sql
+++ src/main/resources/db/migration/V4__Insert_one_question.sql
@@ -1,1 +1,1 @@
-insert into questions (id, word, theme, correct_letters_sequence) values (1,'arroz','COMIDA','aroz');
+insert into questions (id, word, correct_letters_sequence) values (1,'arroz', 'aroz');
