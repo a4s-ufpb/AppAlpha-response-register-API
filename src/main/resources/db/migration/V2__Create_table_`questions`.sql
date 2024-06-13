9b783945dc497c1ba70d5e92b8ef33375872dcc1
André Victor <andrehsvictor@gmail.com>
2024-06-13 13:27:04 -0300

    refactor: Remove theme validation from questions table creation

--- src/main/resources/db/migration/V2__Create_table_`questions`.sql
+++ src/main/resources/db/migration/V2__Create_table_`questions`.sql
@@ -1,16 +1,5 @@
 create table if not exists questions (
     id serial primary key,
     word varchar(255) not null,
-    theme varchar(255) not null,
-    correct_letters_sequence varchar(255) not null,
-    check (
-        theme in (
-            'COMIDA',
-            'CIDADE',
-            'CORES',
-            'COZINHA',
-            'FRUTAS',
-            'NATUREZA'
-        )
-    )
+    correct_letters_sequence varchar(255) not null
 );
