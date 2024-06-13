015b14d037d1888aa6b15c8cfa703bde64da70f3
André Victor <andrehsvictor@gmail.com>
2024-06-13 13:25:54 -0300

    feat: Add CRUD endpoints for Response entity

--- src/main/java/br/dcx/ufpb/apps4society/appalpharesponseregister/controller/ResponseController.java
+++ src/main/java/br/dcx/ufpb/apps4society/appalpharesponseregister/controller/ResponseController.java
@@ -2,10 +2,17 @@ package br.dcx.ufpb.apps4society.appalpharesponseregister.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.dcx.ufpb.apps4society.appalpharesponseregister.dto.ResponseDTO;
 import br.dcx.ufpb.apps4society.appalpharesponseregister.service.ResponseService;
 
 @RestController
@@ -23,5 +30,21 @@ public class ResponseController {
     @GetMapping("/response/{id}")
     public ResponseEntity<?> getResponseById(long id) {
         return ResponseEntity.ok(responseService.getResponseById(id));
+    }
+
+    @PostMapping("/questions/{questionId}/users/{userId}/responses")
+    public ResponseEntity<?> saveResponse(@PathVariable Long userId, @PathVariable Long questionId, @RequestBody ResponseDTO responseDTO) {
+        return ResponseEntity.status(201).body(responseService.saveResponse(userId, questionId, responseDTO));
+    }
+
+    @PutMapping("/responses/{id}")
+    public ResponseEntity<?> updateResponse(@RequestBody ResponseDTO responseDTO) {
+        return ResponseEntity.ok(responseService.updateResponse(responseDTO));
+    }
+
+    @DeleteMapping("/responses/{id}")
+    public ResponseEntity<?> deleteResponse(@PathVariable Long id) {
+        responseService.deleteResponse(id);
+        return ResponseEntity.noContent().build();
     }
 }
